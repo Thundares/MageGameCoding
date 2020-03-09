80d9316a3a3d09e47c3a2e7583ed68a32cdf914b
Rafael Mippo <rkmippo@gmail.com>
2020-03-09 15:56:09 -0300

    Add the first version of the camera controller

---
+++ CameraMovimento.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovimento : MonoBehaviour
+{
+
+    public GameObject Player;
+    private Vector3 Movimento;
+
+    private void Start() 
+    {
+        Movimento = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + Movimento;
+    }
+}
